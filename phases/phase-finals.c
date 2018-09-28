d92357348d8734641371974e60f4ffd0ee7d2a59
ManoMax <gabrielmaxcontato@gmail.com>
2018-09-28 13:17:42 -0300

    Correções de saída

--- phases/phase-finals.c
+++ phases/phase-finals.c
@@ -3,7 +3,7 @@ void phase_finals(struct game *gm){
 
 		struct day *dy = createDay("Você está se aproximando das provas finais do segundo periodo.", 3);
 
-			struct mission *msn = createMission("Você tem uma prova do terceiro estágio de lp2, porém seu desempenho esse periodo não foi tão bom\n e você também tem algumas finais semana que vem. Decida o que você fará!", 3);
+			struct mission *msn = createMission("Você tem uma prova do terceiro estágio de lp2, porém seu desempenho esse periodo não foi tão bom\n\t e você também tem algumas finais semana que vem. Decida o que você fará!", 3);
 
 				struct action *act = createAction("Irei fazer essa prova de qualquer modo pois devo priorizar minhas finais", "Você não foi tão bem na prova já que não estava preparado, mas certamente irá melhor nas finais!", 0 , 0, -5, 0);
 				addActionToMission(msn, act);
@@ -11,13 +11,13 @@ void phase_finals(struct game *gm){
 				act = createAction("Deixarei para repor farei semana que vem junto com as finais", "Você deixa pra repor e deixa os problemas para o seu eu do futuro!", +5, 0, +5, -5);
 				addActionToMission(msn, act);
 
-				act = createAction("Vou pedir conselhos pra uma amiga que estou conhecendo melhor", "A amiga em questão, diz que é melhor fazer a prova e te ajuda a se preparar com algumas dicas encima da hora!", -5, 0, -5, +5);
+				act = createAction("Vou pedir conselhos pra uma amiga que estou conhecendo melhor", "A amiga em questão, diz que é melhor fazer a prova e\n\t te ajuda a se preparar com algumas dicas encima da hora!", -5, 0, -5, +5);
 				addActionToMission(msn, act);
 
 
 		addMissionToDay(dy, msn);
 
-			msn = createMission("Você lembra que o final de semana está chegando e você está andando lado a lado com sua colega, você se recorda\n o quanto ela já te ajudou em provas passadas e como você viu ela pela primeira vez na semana do fera.\n O que você deseja fazer?", 3);
+			msn = createMission("Você lembra que o final de semana está chegando e você está andando lado a lado com sua colega, você se recorda\n\t o quanto ela já te ajudou em provas passadas e como você viu ela pela primeira vez na semana do fera.\n\t O que você deseja fazer?", 3);
 
 				act = createAction("Chamarei ela para uma festa esse final de semana, para que possa conhece-la melhor!", "Ela recusa na hora, -quem pensaria em festa num momento desses!! não imaginava isso de você!-", -5, 0, 0, 0);
 				addActionToMission(msn, act);
@@ -65,12 +65,10 @@ void phase_finals(struct game *gm){
 			act = createAction("Vou tentar fazer de qualquer jeito e correr o risco de reprovar! pelo menos serei honesto!", "Você acaba desenvolvendo mais da questão do que esperava, talvez o professor reconheça seu esforço!", -5, 0, -5, +5);
 			addActionToMission(msn, act);
 
-			act = createAction("Tento olhar a resposta no celular já que deixei os slides preparados por precaução...", "O Professor passa por você enquanto você olha o celular\n, ele aparenta ter percebido porém você esconde\n o celular rápido
-e se safa dessa vez.", -5, 0, 0, 0);
+			act = createAction("Tento olhar a resposta no celular já que deixei os slides preparados por precaução...", "O Professor passa por você enquanto você olha o celular\n\t, ele aparenta ter percebido porém você esconde\n\t o celular rápido e se safa dessa vez.", -5, 0, 0, 0);
 			addActionToMission(msn, act);
 
-			act = createAction("Tento ver a resposta por cima do ombro de alguém do lado...", "Sua amiga de sempre estava atrás de você e percebe sua agonia\n, ela te passa um papel com as respostas e o professor nem
-desconfia\ndeu sorte!", -1, 0, -2, +1);
+			act = createAction("Tento ver a resposta por cima do ombro de alguém do lado...", "Sua amiga de sempre estava atrás de você e percebe sua agonia\n\t, ela te passa um papel com as respostas e o professor nem desconfia\n\tdeu sorte!", -1, 0, -2, +1);
 			addActionToMission(msn, act);
 
 		addMissionToDay(dy, msn);
@@ -78,13 +76,13 @@ desconfia\ndeu sorte!", -1, 0, -2, +1);
 		
 		msn = createMission("Você sai da prova e quando estava indo embora lembra de algo que queria fazer, o que era mesmo?", 3);
 
-			act = createAction("Vou esperar minha amiga sair da prova, hoje eu vou me declarar pra ela!", "Vocês saem e e vão andando embora, no caminho\nvocê a diz como se sente e ela... corresponde!\n Você agora está namorando com Maria Joaquina!", +10, 0, -5, 0);
+			act = createAction("Vou esperar minha amiga sair da prova, hoje eu vou me declarar pra ela!", "Vocês saem e e vão andando embora, no caminho\n\tvocê a diz como se sente e ela... corresponde!\n\t Você agora está namorando com Maria Joaquina!", +10, 0, -5, 0);
 			addActionToMission(msn, act);
 
-			act = createAction("acho melhor ir embora, realmente preciso descansar!.", "No caminho saindo da faculdade, ela te alcança e te diz algo inesperado...\nemocionado você corresponde seus sentimentos. \nVocê agora está namorando Maria Joaquina!", +10, 0, -5, 0);
+			act = createAction("acho melhor ir embora, realmente preciso descansar!.", "No caminho saindo da faculdade, ela te alcança e te diz algo inesperado...\n\temocionado você corresponde seus sentimentos. \n\tVocê agora está namorando Maria Joaquina!", +10, 0, -5, 0);
 			addActionToMission(msn, act);
 
-			act = createAction("Vou espera-la, pelo menos para agradecer por ter me dado a resposta!!", "Você agradece pela resposta e pergunta porquê ela sempre lhe ajuda\n, a resposta não podia te deixar mais feliz, ela gosta de você\n você não perde tempo e também se declara.\nVocê agora está namorando Maria Joaquina!", +10, 0, -5, 0);
+			act = createAction("Vou espera-la, pelo menos para agradecer por ter me dado a resposta!!", "Você agradece pela resposta e pergunta porquê ela sempre lhe ajuda\n\t, a resposta não podia te deixar mais feliz, ela gosta de você\n\t você não perde tempo e também se declara.\n\tVocê agora está namorando Maria Joaquina!", +10, 0, -5, 0);
 			addActionToMission(msn, act);
 
 		addMissionToDay(dy, msn);
