36fcc5c53b6beb5e31313157afe21b5631d7364f
Anderson Kleber Dantas <a.kleber.d@gmail.com>
2018-09-28 01:21:17 -0300

    Implementado condicao de game over

--- main.c
+++ main.c
@@ -3,26 +3,59 @@
 #include "classes.c"
 #include "utils.c"
 #include "phases/phase-marathon-girfriend.c"
+#define SANITY_DEATH 20
+#define MONEY_DEATH -10
+#define ENERGY_DEATH 15
+#define WISDOM_DEATH 15
 
-
-// addPhaseToGameLoop
+int readNumber(int bound){
+	char c=0;
+	while(c < '1' || c > '9') {
+		if(c == 27 || c == 24 || c == 3 || c == 4) exit(0);
+		c=getchar();
+		printf("Digite uma opcao valida\n");
+	}
+	return c-1-'0';
+}
 
 int main() {
+	// system ("/bin/stty raw");
 	struct game *gm = createGame(5, 80, 80, 80, 80);
 	phase_marathon_girfriend(gm);
-
 	for(int i = 0; i < gm->total; i++){
 		struct phase *ph = &gm->phases[i];
 		for(int j = 0; j < ph->total; j++){
 			struct day *dy = &ph->days[j];
 			for(int k = 0; k < dy->total; k++){
 				struct mission *msn = &dy->missions[k];
+				printf("Define o numero referente a sua decisao:\n\n");
 				for(int l = 0; l < msn->total; l++){
 					struct action *act = &msn->actions[l];
 					printf("%d) %s\n", l+1, act->definition);
 					// printf(" %s\n", act->consequence);
 				}
-				// getchar();
+				int option = readNumber(msn->total);
+				printf("\n\nA consequencia da sua decisao foi:\n\n");
+				printf("%s\n\n\n", msn->actions[option].consequence);
+				system("read -p \"pressione qualquer tecla para continuar\" saindo");
+				printf("\n\n-------------------------------\n\n");
+				gm->attr[sanity] += msn->actions[option].changes[sanity];
+				gm->attr[money] += msn->actions[option].changes[money];
+				gm->attr[energy] += msn->actions[option].changes[energy];
+				gm->attr[wisdom] += msn->actions[option].changes[wisdom];
+				if(gm->attr[sanity] < SANITY_DEATH || gm->attr[money] < MONEY_DEATH || gm->attr[energy] < ENERGY_DEATH || gm->attr[wisdom] < WISDOM_DEATH){
+					// brek loops
+					i = j = k = 1e9;
+
+					if(gm->attr[sanity] < SANITY_DEATH)
+						printf("Seus pontos de sanidade atingiram um nível muito baixo. Você\n\t enlouqueceu e acabou pulando do terceiro andar do CAA\n");
+					else if(gm->attr[money] < MONEY_DEATH)
+						printf("Voce acabou fazendo dividas demais. seu dinheiro acaou e voce \n\t acabou precisando largar a faculdade para pagar as contas \n\t com o agiota antes que ele te mate.\n");
+					else if(gm->attr[energy] < ENERGY_DEATH)
+						printf("Voce esta sem energia. Muito fraco e debilitado, \n\t acabou no hospital para se recuperar e no fim das contas\n\t perdeu esse periodo completamente");
+					else
+						printf("Voce esta estudando de maneira errada ou nao estudando \n\tde qualquer forma. suas notas cairam ao ponto em que a coordenador \n\tachou sensato te remover fisicamente do curso para\n\t abrir vaga para alguem mais dedicado\n");
+				}
 			}
 		}
 	}
--- phases/phase-marathon-girfriend.c
+++ phases/phase-marathon-girfriend.c
@@ -14,11 +14,23 @@ void phase_marathon_girfriend(struct game *gm){
 				act = createAction("Interrompe a explicacao, explica o problema que esta tendo \n\te solicita que o prof explique melhor como que essa coisa funciona", "Dalbas não gostou de ser interrompido. Voce leva um esporro \n\tna frente da turma toda e ele explica resmungando. Voce entende uma parte. \n\tainda restam duvidas mas voce decide nao incomodar mais", -2, 0, 0, +4);
 				addActionToMission(msn, act);
 
-				act = createAction("Voce acha que esse assunto nunca vai te fazer falta resolve\n\tse aconchegar melhor na cadeira e tirar uma soneca por que pqp, aula de 8h? isso nao e coisa de gente", "voce dorme como um condenado e acorda no fim da aula as energias revigoradas", +2, 0, +7, -3);
+				act = createAction("Voce acha que esse assunto nunca vai te fazer falta resolve\n\tse aconchegar melhor na cadeira e tirar uma soneca por que pqp,\n\taula de 8h? isso nao e coisa de gente", "voce dorme como um condenado e acorda no fim da aula as energias revigoradas", +2, 0, +7, -3);
 				addActionToMission(msn, act);
 
 
-		addMissionToDay(dy, msn);
-	addDayToPhase(ph, dy);
+			addMissionToDay(dy, msn);
+
+			msn = createMission("Saindo da aula voce encontra com Maria Joaquina, sua tao amada namorada. \n\t ela te pergunta onde vao almocar", 3);
+				act = createAction("bora pro estaurante universitario, mesmo com aquela fila e sendo ruim a \n\t comida, e de graca acima de tudo.", "A maria Joaquina faz cara feia mas aceita ir para lá com Voce", -3, 0, -3, 0);
+				addActionToMission(msn, act);
+
+				act = createAction("Vamo comer aquele pastelao la de seu olavo.", "A maria Joaquina reluta em aceitar ate pq isso nao e comida\n\t de almoco mas aquele pastel e muito bom", 3, -2, +3, 0);
+				addActionToMission(msn, act);
+
+				act = createAction("Acabei de ganhar minha mesada. Vamos comer aquele meio almoco top\n\t", "A Maria Joaquina aceita com um sorriso brilhante. Voce depois\n\t se da conta do rombo que fez na sua carteira e diz pra si proprio \n\tque vai ser mais responsavel com as financas", 3, -2, +3, 0);
+				addActionToMission(msn, act);
+			addMissionToDay(dy, msn);
+
+		addDayToPhase(ph, dy);
 	addPhaseToGameLoop(gm, ph);
 }
