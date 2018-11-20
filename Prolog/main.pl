2c6f507053d6eeab404d005b874e626dbc52edbb
Anderson Dantas <a.kleber.d@gmail.com>
2018-11-20 00:50:13 -0300

    Implementa entrada para escolha escolha de decisão

--- Prolog/main.pl
+++ Prolog/main.pl
@@ -1,17 +1,22 @@
-lerNumero(X) :-
-	read_line_to_codes(user_input, Z),
-	string_to_atom(Z, A),
-	atom_number(A, X).
 
 dynamic(wisdom/1).
 dynamic(money/1).
 dynamic(sanity/1).
 dynamic(energy/1).
 
+lerNumero(X) :-
+	% read_line_to_codes(user_input, Z),
+	% string_to_atom(Z, A),
+	% atom_number(A, X).
+	read(Y),
+	X is Y-1.
+
 wisdom(80).
 money(80).
 sanity(80).
 energy(80).
+
+
 
 % :- wisdom(X), Y is 8+X, retract(wisdom(X)), asserta(wisdom(Y)).
 % :- wisdom(X), write(X).
@@ -22,10 +27,33 @@ updateAttr([Wis, Mon, San, En]) :-
 	wisdom(S), Sn is (San+S), asserta(wisdom(Sn)),
 	wisdom(E), Eg is (En+E),  asserta(wisdom(Eg)).
 
+prt(Des) :- write(Des), nl.
+
 :- initialization(main).
 
-main :- lerNumero(X), Y is X+1, write(Y), nl.	
+showActions([], _).
+showActions([acao(Txt,_,_)|T], Idx) :- 
+	format("~d) ~w\n", [Idx, Txt]),
+	Ids is (Idx+1), 
+	showActions(T, Ids).
 
+main :- 
+	
+	mission(Desc, Actions),
+	prt(Desc),	
+	showActions(Actions, 1),
+	% 	member(acao(Txt, _, _), Actions) ->
+	% prt(Txt)
+	lerNumero(X),
+	% prt(X).
+	% member(acao(Txt, _, _), Actions),
+	
+	% nth0(Y, Actions, acao(Txt, Cons, Attr)),
+	% prt(Txt),
+	% prt(Cons),
+	% updateAttr(Attr),
+	% wisdom(C), prt(C).
+	halt.
 
 mission(
 	"Ao iniciar o Curso, todos tem que passar pelo\n\tprocesso de montagem de grade. Desse modo, decida\n\tquais cadeiras escolher.",
